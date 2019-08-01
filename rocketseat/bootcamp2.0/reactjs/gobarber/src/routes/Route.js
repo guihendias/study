10af8b1e2b6d93978756eacd51aa351a80d0d941
Guilherme Dias <guilhermehenriquedias@hotmail.com>
2019-08-01 14:38:16 -0300

    GoBarber web

--- rocketseat/bootcamp2.0/reactjs/gobarber/src/routes/Route.js
+++ rocketseat/bootcamp2.0/reactjs/gobarber/src/routes/Route.js
@@ -5,12 +5,14 @@ import { Route, Redirect } from 'react-router-dom';
 import AuthLayout from '~/pages/_layouts/auth';
 import DefaultLayout from '~/pages/_layouts/default';
 
+import { store } from '~/store';
+
 export default function RouteWrapper({
   isPrivate,
   component: Component,
   ...rest
 }) {
-  const signed = false;
+  const { signed } = store.getState().auth;
 
   if (!signed && isPrivate) {
     return <Redirect to="/" />;
