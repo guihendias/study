e21b10d4a769a07b2e437d744e986fee1df5d702
Guilherme Dias <guilhermehenriquedias@hotmail.com>
2019-08-06 21:40:39 -0300

    Semana Omnistack 8 back-end

---
+++ rocketseat/omnistack/weekeight/backend/src/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const routes = require("./routes");
+
+const server = express();
+
+mongoose.connect(
+  "mongodb+srv://omnistack:omnistack@cluster0-xqysk.mongodb.net/omnistack8?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true
+  }
+);
+
+server.use(express.json());
+server.use(cors());
+server.use(routes);
+
+server.listen(3333);
